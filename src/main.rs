b31f5b0b33d47ba7f97a4f1280e620c3e39ab145
sanlee42 <hit.cs.lijun@gmail.com>
2025-05-06 17:12:13 -0600

    export to web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use starcoin_graphd::graph::DagGraph;
+use starcoin_graphd::chain_reader::mock::MockChainReader;
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let reader = MockChainReader::new();
+    let graph = DagGraph::build(&reader, None, 2).await?;
+
+    let json = serde_json::to_string_pretty(&graph)?;
+    std::fs::write("dag.json", json)?;
+    Ok(())
 }
